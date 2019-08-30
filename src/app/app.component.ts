1f3b1f169a751ae9698a18bb03079f045642cd76
Jackson Santos <jackson.santos@aptasistemas.local>
2019-08-30 15:48:31 -0300

    Primeiro commit cap 2 import bootstrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  description = 'Deadpool';
+  url ='https://www.diariodepernambuco.com.br/static/app/noticia_127983242361/2016/02/11/626401/20160211102359155036u.jpg';
+}
